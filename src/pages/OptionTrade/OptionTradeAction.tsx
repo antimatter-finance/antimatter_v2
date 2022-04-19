e31ec0bde6eaed1ff5e8ff2f401dbfb364e531f6
7wei <cwhuang0523@gmail.com>
2022-04-19 22:45:38 +0800

    remove unused

--- src/pages/OptionTrade/OptionTradeAction.tsx
+++ src/pages/OptionTrade/OptionTradeAction.tsx
@@ -1,14 +1,14 @@
-import React, { useCallback, useState } from 'react'
-import { ChevronLeft } from 'react-feather'
-import { useHistory } from 'react-router'
+import React from 'react'
+// import { ChevronLeft } from 'react-feather'
+// import { useHistory } from 'react-router'
 import styled from 'styled-components'
 import OptionSwap from './OptionSwap'
 import AppBody, { BodyWrapper } from 'pages/AppBody'
 import { CustomLightSpinner, ExternalLink, TYPE, HideSmall, ShowSmall } from 'theme'
 //import Liquidity from './Liquidity'
-import useTheme from 'hooks/useTheme'
+// import useTheme from 'hooks/useTheme'
 import { RowBetween, RowFixed } from 'components/Row'
-import { ButtonEmpty } from 'components/Button'
+// import { ButtonEmpty } from 'components/Button'
 import { AutoColumn } from 'components/Column'
 //import { USDT } from '../../constants'
 //import { useCurrency } from 'hooks/Tokens'
@@ -26,11 +26,11 @@ import { useCurrencyBalance } from '../../state/wallet/hooks'
 //import { ChainId, WETH } from '@uniswap/sdk'
 //import { useDerivedMintInfo } from 'state/mint/hooks'
 
-enum TABS {
-  SWAP = 'swap',
-  LIQUIDITY = 'liquidity',
-  INFO = 'info'
-}
+// enum TABS {
+//   SWAP = 'swap',
+//   LIQUIDITY = 'liquidity',
+//   INFO = 'info'
+// }
 
 const Wrapper = styled.div`
   min-height: calc(100vh - ${({ theme }) => theme.headerHeight + ' - 100px'});
@@ -62,13 +62,13 @@ const Elevate = styled.div`
   height: 100%;
 `
 
-const SwitchTabWrapper = styled.div`
-  display: flex;
-  margin-bottom: -1px;
-  margin-left: -1px;
-  flex-direction: row-reverse;
-  justify-content: flex-end;
-`
+// const SwitchTabWrapper = styled.div`
+//   display: flex;
+//   margin-bottom: -1px;
+//   margin-left: -1px;
+//   flex-direction: row-reverse;
+//   justify-content: flex-end;
+// `
 
 const Circle = styled.div`
   margin-right: 16px;
@@ -84,42 +84,41 @@ const Circle = styled.div`
   justify-content: center;
 `
 
-const TabStyle = styled.button<{ selected?: boolean; isFirstChild?: boolean }>`
-  outline: none;
-  border:none;
-  background: none;
-  position: relative;
-  min-width: ${({ isFirstChild }) => (isFirstChild ? '157px' : '229px')};
-  padding: 0;
-  height: 42px;
-  margin-left:${({ isFirstChild }) => (isFirstChild ? '0' : '-74px')};
-  svg {
-    z-index:${({ selected }) => (selected ? '2' : '0')}
-    position: absolute;
-    top: ${({ selected }) => (selected ? '0' : '-1px')};
-    left: 0;
-    stroke: ${({ theme, selected }) => (selected ? theme.text4 : theme.text5)};
-    fill:${({ selected, theme, isFirstChild }) => (selected ? '#141414' : isFirstChild ? 'transparent' : theme.bg1)}
-    stroke-width: 1px;
-  }
-  div{
-    z-index: 2;
-    position: absolute;
-    line-height: 41px;
-    text-align: center;
-    top: 0;
-    left:${({ isFirstChild }) => (isFirstChild ? '50px' : '50%')};
-    transform:${({ isFirstChild }) => (isFirstChild ? 'translateX(0)' : 'translateX(-50%)')};
-    margin-left:${({ isFirstChild }) => (isFirstChild ? '0' : '8px')};
-    color: ${({ selected, theme }) => (selected ? theme.text1 : theme.text3)};
-  };
-`
+// const TabStyle = styled.button<{ selected?: boolean; isFirstChild?: boolean }>`
+//   outline: none;
+//   border:none;
+//   background: none;
+//   position: relative;
+//   min-width: ${({ isFirstChild }) => (isFirstChild ? '157px' : '229px')};
+//   padding: 0;
+//   height: 42px;
+//   margin-left:${({ isFirstChild }) => (isFirstChild ? '0' : '-74px')};
+//   svg {
+//     z-index:${({ selected }) => (selected ? '2' : '0')}
+//     position: absolute;
+//     top: ${({ selected }) => (selected ? '0' : '-1px')};
+//     left: 0;
+//     stroke: ${({ theme, selected }) => (selected ? theme.text4 : theme.text5)};
+//     fill:${({ selected, theme, isFirstChild }) => (selected ? '#141414' : isFirstChild ? 'transparent' : theme.bg1)}
+//     stroke-width: 1px;
+//   }
+//   div{
+//     z-index: 2;
+//     position: absolute;
+//     line-height: 41px;
+//     text-align: center;
+//     top: 0;
+//     left:${({ isFirstChild }) => (isFirstChild ? '50px' : '50%')};
+//     transform:${({ isFirstChild }) => (isFirstChild ? 'translateX(0)' : 'translateX(-50%)')};
+//     margin-left:${({ isFirstChild }) => (isFirstChild ? '0' : '8px')};
+//     color: ${({ selected, theme }) => (selected ? theme.text1 : theme.text3)};
+//   };
+// `
 
-const StyledAppBody = styled(BodyWrapper)<{ tab: TABS }>`
+const StyledAppBody = styled(BodyWrapper)`
   max-width: 1116px;
   padding: 0;
   background: black;
-  border-color: ${({ tab }) => (tab === TABS.SWAP ? '#727272' : 'rgb(114, 114, 114)')};
   width: 1116px;
   overflow: hidden;
   ${({ theme }) => theme.mediaWidth.upToLarge`
@@ -182,7 +181,7 @@ const InfoAppBodyInner = styled(BodyWrapper)`
 
 const Title = styled(TYPE.subHeader)`
   white-space: nowrap;
-  ${({ theme }) => theme.mediaWidth.upToSmall`  
+  ${({ theme }) => theme.mediaWidth.upToSmall`
     white-space: pre-wrap;
     padding-right: 14px;
     text-align: right
@@ -199,17 +198,17 @@ const TitleWrapper = styled(AutoColumn)`
 
 export default function OptionTradeAction({ optionId }: { optionId?: string }) {
   //const { chainId } = useActiveWeb3React()
-  const [tab, setTab] = useState(TABS.SWAP)
+  // const [tab, setTab] = useState(TABS.SWAP)
   const option = useOption(optionId)
-  const theme = useTheme()
-  const history = useHistory()
+  // const theme = useTheme()
+  // const history = useHistory()
   //const currencyA = chainId === ChainId.MAINNET ? USDT : chainId && WETH[chainId]
   //const currencyB = useCurrency(addressA)
   //const underlyingCurrency = useCurrency(option?.underlyingAddress ?? undefined)
   //const { pair } = useDerivedMintInfo(currencyA ?? undefined, currencyB ?? undefined)
 
-  const handleSetTab = useCallback((tab: TABS) => setTab(tab), [setTab])
-  const handleBack = useCallback(() => history.push('/option_trading'), [history])
+  // const handleSetTab = useCallback((tab: TABS) => setTab(tab), [setTab])
+  // const handleBack = useCallback(() => history.push('/option_trading'), [history])
 
   const optionPrice = useOptionPrice(option)
 
@@ -218,10 +217,10 @@ export default function OptionTradeAction({ optionId }: { optionId?: string }) {
       {optionId ? (
         <Wrapper>
           <RowBetween style={{ padding: '27px 0', maxWidth: 1116 }}>
-            <ButtonEmpty width="auto" color={theme.text1} onClick={handleBack} padding="14px 14px 14px 0">
-              <ChevronLeft style={{ flexShrink: 0 }} />
-              <HideSmall>Go Back</HideSmall>
-            </ButtonEmpty>
+            {/* <ButtonEmpty width="auto" color={theme.text1} onClick={handleBack} padding="14px 14px 14px 0"> */}
+            {/* <ChevronLeft style={{ flexShrink: 0 }} /> */}
+            {/* <HideSmall>Go Back</HideSmall> */}
+            {/* </ButtonEmpty> */}
 
             <TitleWrapper gap="8px">
               {option && (
@@ -270,14 +269,11 @@ export default function OptionTradeAction({ optionId }: { optionId?: string }) {
             </HideSmall>
           </RowBetween>
           <ActionWrapper>
-            <SwitchTab tab={tab} setTab={handleSetTab} />
-            <StyledAppBody tab={tab} isCard={false}>
+            {/* <SwitchTab tab={tab} setTab={handleSetTab} /> */}
+            <StyledAppBody isCard={false}>
               <Elevate>
-                {tab === TABS.SWAP && (
-                  <OptionSwap /*handleOptionType={setOptionType}*/ optionPrice={optionPrice} option={option} />
-                )}
+                <OptionSwap /*handleOptionType={setOptionType}*/ optionPrice={optionPrice} option={option} />
                 {/*{tab === TABS.LIQUIDITY && <Liquidity currencyA={currencyA} currencyB={currencyB} pair={pair} />}*/}
-                {tab === TABS.INFO && <Info optionPrice={optionPrice} option={option} />}
               </Elevate>
             </StyledAppBody>
           </ActionWrapper>
@@ -291,64 +287,64 @@ export default function OptionTradeAction({ optionId }: { optionId?: string }) {
   )
 }
 
-function SwitchTab({ tab, setTab }: { tab: TABS; setTab: (tab: TABS) => void }) {
-  const swapClick = useCallback(() => setTab(TABS.SWAP), [setTab])
-  //const liquidityClick = useCallback(() => setTab(TABS.LIQUIDITY), [setTab])
-  const infoClick = useCallback(() => setTab(TABS.INFO), [setTab])
-  return (
-    <SwitchTabWrapper>
-      <Tab onClick={infoClick} selected={tab === TABS.INFO}>
-        Info
-      </Tab>
-      {/*<Tab onClick={liquidityClick} selected={tab === TABS.LIQUIDITY}>*/}
-      {/*  Liquidity*/}
-      {/*</Tab>*/}
-      <Tab isFirstChild={true} onClick={swapClick} selected={tab === TABS.SWAP}>
-        Trade
-      </Tab>
-    </SwitchTabWrapper>
-  )
-}
-function Tab({
-  isFirstChild,
-  selected,
-  children,
-  onClick
-}: {
-  isFirstChild?: boolean
-  selected?: boolean
-  children: string
-  onClick: () => void
-}) {
-  return (
-    <TabStyle selected={selected} isFirstChild={isFirstChild} onClick={onClick}>
-      {isFirstChild ? (
-        <svg width="157" height="76" viewBox="0 0 157 76" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M157 42V41.5C150.187 42 143.885 38.3867 140.443 32.5068L127.793 10.8964C124.205 4.76683 117.635 1 110.533 1H33C15.3269 1 1 15.3269 1 33V76" />
-          {!selected && (
-            <path
-              d="M157 42V42C150.187 42 143.885 38.3867 140.443 32.5068L127.793 10.8964C124.205 4.76683 117.635 1 110.533 1H33C15.3269 1 1 15.3269 1 33V42"
-              fill="#000000"
-              strokeWidth="0"
-              stroke="transparent"
-            />
-          )}
-        </svg>
-      ) : (
-        <svg width="229" height="43" viewBox="0 0 229 43" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12.8469 42C25.0603 42 36.2086 35.0479 41.5827 24.0804L44.1114 18.9196C49.4854 7.95208 60.6338 1 72.8472 1H161.754C169.37 1 176.325 5.32574 179.693 12.157L188.904 30.843C192.272 37.6742 199.227 42 206.843 41.5" />
+// function SwitchTab({ tab, setTab }: { tab: TABS; setTab: (tab: TABS) => void }) {
+//   const swapClick = useCallback(() => setTab(TABS.SWAP), [setTab])
+//   //const liquidityClick = useCallback(() => setTab(TABS.LIQUIDITY), [setTab])
+//   const infoClick = useCallback(() => setTab(TABS.INFO), [setTab])
+//   return (
+//     <SwitchTabWrapper>
+//       <Tab onClick={infoClick} selected={tab === TABS.INFO}>
+//         Info
+//       </Tab>
+//       {/*<Tab onClick={liquidityClick} selected={tab === TABS.LIQUIDITY}>*/}
+//       {/*  Liquidity*/}
+//       {/*</Tab>*/}
+//       <Tab isFirstChild={true} onClick={swapClick} selected={tab === TABS.SWAP}>
+//         Trade
+//       </Tab>
+//     </SwitchTabWrapper>
+//   )
+// }
+// function Tab({
+//   isFirstChild,
+//   selected,
+//   children,
+//   onClick
+// }: {
+//   isFirstChild?: boolean
+//   selected?: boolean
+//   children: string
+//   onClick: () => void
+// }) {
+//   return (
+//     <TabStyle selected={selected} isFirstChild={isFirstChild} onClick={onClick}>
+//       {isFirstChild ? (
+//         <svg width="157" height="76" viewBox="0 0 157 76" fill="none" xmlns="http://www.w3.org/2000/svg">
+//           <path d="M157 42V41.5C150.187 42 143.885 38.3867 140.443 32.5068L127.793 10.8964C124.205 4.76683 117.635 1 110.533 1H33C15.3269 1 1 15.3269 1 33V76" />
+//           {!selected && (
+//             <path
+//               d="M157 42V42C150.187 42 143.885 38.3867 140.443 32.5068L127.793 10.8964C124.205 4.76683 117.635 1 110.533 1H33C15.3269 1 1 15.3269 1 33V42"
+//               fill="#000000"
+//               strokeWidth="0"
+//               stroke="transparent"
+//             />
+//           )}
+//         </svg>
+//       ) : (
+//         <svg width="229" height="43" viewBox="0 0 229 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+//           <path d="M12.8469 42C25.0603 42 36.2086 35.0479 41.5827 24.0804L44.1114 18.9196C49.4854 7.95208 60.6338 1 72.8472 1H161.754C169.37 1 176.325 5.32574 179.693 12.157L188.904 30.843C192.272 37.6742 199.227 42 206.843 41.5" />
 
-          <path
-            stroke="transparent"
-            fill={selected ? '#141414' : 'transparent'}
-            d="M12.8469 43C25.0603 42 36.2086 35.0479 41.5827 24.0804L44.1114 18.9196C49.4854 7.95208 60.6338 1 72.8472 1H161.754C169.37 1 176.325 5.32574 179.693 12.157L188.904 30.843C192.272 37.6742 199.227 42 206.843 43"
-          />
-        </svg>
-      )}
-      <TYPE.smallHeader fontSize={18}>{children}</TYPE.smallHeader>
-    </TabStyle>
-  )
-}
+//           <path
+//             stroke="transparent"
+//             fill={selected ? '#141414' : 'transparent'}
+//             d="M12.8469 43C25.0603 42 36.2086 35.0479 41.5827 24.0804L44.1114 18.9196C49.4854 7.95208 60.6338 1 72.8472 1H161.754C169.37 1 176.325 5.32574 179.693 12.157L188.904 30.843C192.272 37.6742 199.227 42 206.843 43"
+//           />
+//         </svg>
+//       )}
+//       <TYPE.smallHeader fontSize={18}>{children}</TYPE.smallHeader>
+//     </TabStyle>
+//   )
+// }
 
 export function Info({
   option,
